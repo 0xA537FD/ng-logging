6416f00e0c10b3a726f3b185cc44b64109e3ee87
Peter Morawski <contact@peter-morawski.de>
2019-05-20 21:02:12 +0200

    * update the package config of the library

--- projects/ngx-logging/src/lib/logging.service.spec.ts
+++ projects/ngx-logging/src/lib/logging.service.spec.ts
@@ -18,4 +18,40 @@ describe('LoggingService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should format the INFO message');
+
+  it('should format the INFO message with an object in args');
+
+  it('should stringify an object as the INFO message');
+
+  it('should NOT format the INFO message when using an object as a message');
+
+  it('should format the WARN message');
+
+  it('should format the WARN message with an object in the args');
+
+  it('should stringify an object as the WARN message');
+
+  it('should NOT format the WARN message when using an object as a message');
+
+  it('should format the ERROR message');
+
+  it('should format the ERROR message with an object in the args');
+
+  it('should stringify an object as the ERROR message');
+
+  it('should NOT format the ERROR message when using an object as a message');
+
+  it('should NOT log DEBUG when its not active');
+
+  it('should log DEBUG when its active');
+
+  it('should format the DEBUG message');
+
+  it('should format the DEBUG message with an object in the args');
+
+  it('should stringify an object as the DEBUG message');
+
+  it('should NOT format the DEBUG message when using an object as a message');
 });
