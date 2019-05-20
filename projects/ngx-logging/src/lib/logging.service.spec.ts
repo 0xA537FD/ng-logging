334bc09277e06977ea8e0e799db805ce4c8ea87b
Peter Morawski <contact@peter-morawski.de>
2019-05-20 21:36:16 +0200

    * implement the info logging and the appropriate tests for it

--- projects/ngx-logging/src/lib/logging.service.spec.ts
+++ projects/ngx-logging/src/lib/logging.service.spec.ts
@@ -1,15 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 
-import { LoggingService } from './logging.service';
+import { INFO_PREFIX, LoggingService } from './logging.service';
 import { RouterTestingModule } from '@angular/router/testing';
+import { LOGGING_CONFIG } from './config-token';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Observer } from 'rxjs';
 
 describe('LoggingService', () => {
   let service: LoggingService;
+  let fakeActivatedRoute: any;
+  let paramsObserver: Observer<any>;
 
   beforeEach(() => {
+    fakeActivatedRoute = {
+      queryParams: Observable.create(observer => (paramsObserver = observer)),
+    };
+
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      providers: [LoggingService],
+      providers: [
+        LoggingService,
+        { provide: LOGGING_CONFIG, useValue: {} },
+        { provide: ActivatedRoute, useValue: fakeActivatedRoute },
+      ],
     });
 
     service = TestBed.get(LoggingService);
@@ -19,13 +32,57 @@ describe('LoggingService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should format the INFO message');
+  it('should format the INFO message', () => {
+    const spy = spyOn(window.console, 'log');
+    service.info(
+      'some {} which {} {} logged {} {}',
+      'message',
+      'should',
+      'be',
+      12,
+      false
+    );
 
-  it('should format the INFO message with an object in args');
+    expect(spy).toHaveBeenCalledWith(
+      INFO_PREFIX + 'some message which should be logged 12 false'
+    );
+  });
 
-  it('should stringify an object as the INFO message');
+  it('should format the INFO message with an object in args', () => {
+    const spy = spyOn(window.console, 'log');
+    const o = {
+      name: 'Peter',
+      type: 'git',
+    };
+    service.info('some message with an {}', o);
 
-  it('should NOT format the INFO message when using an object as a message');
+    expect(spy).toHaveBeenCalledWith(
+      INFO_PREFIX + 'some message with an ' + JSON.stringify(o)
+    );
+  });
+
+  it('should stringify an object as the INFO message', () => {
+    const spy = spyOn(window.console, 'log');
+    const o = {
+      status: 12,
+      enabled: false,
+      message: ':(){:|:&};:',
+    };
+    service.info(o);
+
+    expect(spy).toHaveBeenCalledWith(INFO_PREFIX + JSON.stringify(o));
+  });
+
+  it('should NOT format the INFO message when using an object as a message', () => {
+    const spy = spyOn(window.console, 'log');
+    const o = {
+      me: 'bread',
+      t: 1337,
+    };
+    service.info(o, 'some', 'random', 'params', 13, true, true);
+
+    expect(spy).toHaveBeenCalledWith(INFO_PREFIX + JSON.stringify(o));
+  });
 
   it('should format the WARN message');
 
--- projects/ngx-logging/src/lib/logging.service.ts
+++ projects/ngx-logging/src/lib/logging.service.ts
@@ -1,8 +1,56 @@
 import { Inject, Injectable } from '@angular/core';
 import { LOGGING_CONFIG } from './config-token';
 import { LoggingConfig } from './ds/logging-config';
+import * as format_ from 'string-format';
+const format = format_;
+
+export const INFO_PREFIX = '[INFO] ';
+export const WARN_PREFIX = '[WARN] ';
+export const ERROR_PREFIX = '[ERROR] ';
+export const DEBUG_PREFIX = '[DEBUG] ';
 
 @Injectable()
 export class LoggingService {
   constructor(@Inject(LOGGING_CONFIG) private _config: LoggingConfig) {}
+
+  info(
+    message: string | number | boolean | object,
+    ...args: Array<string | number | boolean | object>
+  ) {
+    console.log(INFO_PREFIX + this._buildLogMessage(message, ...args));
+  }
+
+  private _buildLogMessage(
+    message: string | number | boolean | object,
+    ...args: Array<string | number | boolean | object>
+  ) {
+    if (typeof message === 'string') {
+      return format(message, ...args.map(x => this._buildLogMessage(x)));
+    } else if (typeof message === 'object') {
+      return JSON.stringify(message);
+    }
+
+    return message.toString();
+  }
+
+  warn(
+    message: string | number | boolean | object,
+    ...args: Array<string | number | boolean | object>
+  ) {
+    // TODO(pmo): Needs to be implemented
+  }
+
+  error(
+    message: string | number | boolean | object,
+    ...args: Array<string | number | boolean | object>
+  ) {
+    // TODO(pmo): Needs to be implemented
+  }
+
+  debug(
+    message: string | number | boolean | object,
+    ...args: Array<string | number | boolean | object>
+  ) {
+    // TODO(pmo): Needs to be implemented
+  }
 }
